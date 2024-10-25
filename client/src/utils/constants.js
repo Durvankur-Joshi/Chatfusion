e8ccfd7f4a29da5ba171f654670073b9acbdebf3
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2024-10-25 20:59:05 +0530

    working on the client routes

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -5,3 +5,4 @@ export const LOGIN_ROUTES = `${AUTH_ROUTES}/login`;
 export const GET_USER_INFO = `${HOST}${AUTH_ROUTES}/user-info`;
 export const UPDATE_PROFILE_ROUTES = `${HOST}${AUTH_ROUTES}/update-profile`;
 export const ADD_PROFILE_IMAGE_ROUTES = `${HOST}${AUTH_ROUTES}/add-profile-image`;
+export const LOGOUT_ROUTES = `${AUTH_ROUTES}/logout`
