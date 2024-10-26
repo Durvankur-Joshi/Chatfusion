88954e102439d61ff41b786b23c482e957d5c789
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2024-10-26 22:24:47 +0530

    contact Controller work done

---
+++ server/controllers/contactController.js
@@ -0,0 +1,34 @@
+import User from "../models/User";
+
+export const searchContact = async (req, res, next) => {
+    try {
+      
+         const {searchTerm}= req.body;
+
+         if(searchTerm === undefined || searchTerm === null)
+         {
+            return res.status(400).send("searchTerm is required");
+         }
+
+         const sanitizedSearchTerm = searchTerm.replace(
+            /[.*+?^{}()|[\]\\]/9,
+            "\\$&"
+         )
+
+         const regex = new RegExp(sanitizedSearchTerm , "i");
+
+         const contacts = await User.find({
+            $and:[
+                {_id:{$ne:req.userId}},
+                {
+                    $or:[{firstname:regex} , {lastname:regex} , {email:regex}]
+                },
+            ]
+         })
+
+     return res.status(200).send("Logout Successful");
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send("Internal server error");
+    }
+  };
