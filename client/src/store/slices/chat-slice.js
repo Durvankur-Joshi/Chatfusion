f2695e156f95a172ab380333abd5456c2809932e
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2024-10-28 09:41:29 +0530

    close chat functionality

--- client/src/store/slices/chat-slice.js
+++ client/src/store/slices/chat-slice.js
@@ -3,22 +3,12 @@ export const createChatSlice = (set, get) => ({
     selectedChatType: undefined,
     selectedChatData: undefined,
     selectedChatMessage: [],
-    // setSelectedChatType: (selectedChatType) => set({ selectedChatType }), // corrected function name
-
-    setSelectedChatType: (selectedChatType) => {
-        console.log("Setting selectedChatType:", selectedChatType);
-        set({ selectedChatType });
-    },
-
-    // setSelectedChatData: (selectedChatData) => set({ selectedChatData }), // corrected function name
-
-
-    setSelectedChatData: (selectedChatData) => {
-        console.log("Setting selectedChatData:", selectedChatData);
-        set({ selectedChatData });
-    },
-
-
+    setSelectedChatType: (selectedChatType) => set({ selectedChatType }), // corrected function name
+    setSelectedChatData: (selectedChatData) => set({ selectedChatData }), // corrected function name
     setSelectedChatMessage: (selectedChatMessage) => set({ selectedChatMessage }),
-   
+    closeChat: () => set({
+        selectedChatType: undefined,
+        selectedChatData: undefined,
+        selectedChatMessage: [],
+    }),
 });
