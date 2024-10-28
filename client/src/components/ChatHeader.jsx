03cb5c89fb80f97c0cda899a2da39401db9a3043
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2024-10-28 09:41:03 +0530

    close chat functionality

--- client/src/components/ChatHeader.jsx
+++ client/src/components/ChatHeader.jsx
@@ -1,12 +1,17 @@
+// ChatHeader.js
 import React from 'react';
+import { useAppStore } from '../store';
 
 const ChatHeader = () => {
+  const { closeChat } = useAppStore(); // Access selectedContact here
+
   return (
     <div className="bg-gray-900 p-4 flex flex-col md:flex-row items-center justify-between text-white">
-      <h2 className="text-lg md:text-xl font-bold">Chat with Username</h2>
+      <h2 className="text-lg md:text-xl font-bold">Chat with </h2>
       <div className="flex flex-wrap space-x-0 md:space-x-4 mt-2 md:mt-0">
-        <button className="hover:bg-slate-800 text-blue-500 px-4 py-2 rounded-full">Video Call</button>
-        <button className="hover:bg-slate-800 text-blue-500 px-4 py-2 rounded-full">More</button>
+        
+      <button className="btn btn-sm btn-circle btn-ghost absolute right-5  top-3" onClick={closeChat}>✕</button>
+       
       </div>
     </div>
   );
