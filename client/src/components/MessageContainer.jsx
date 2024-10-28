43f3f4cdfd9fc5b1b4feef3b666d48c0b1c13270
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2024-10-28 14:09:40 +0530

    Chat header component work is done

--- client/src/components/MessageContainer.jsx
+++ client/src/components/MessageContainer.jsx
@@ -5,16 +5,22 @@ const MessageContainer = () => {
     <div className="flex-1 h-full p-4 bg-gray-900 overflow-y-auto">
       <div className="flex flex-col space-y-4">
         {/* Sent Message */}
+        <div className="flex justify-start">
+          <div className="bg-gray-800 text-white p-3 rounded-lg max-w-xs md:max-w-md">
+            <p>HI ,  naam bataya  </p>
+          </div>
+        </div>
+        
         <div className="flex justify-end">
           <div className="bg-blue-700 text-white p-3 rounded-lg max-w-xs md:max-w-md">
-            <p>Hello, how are you?</p>
+            <p>bhupendra jogi!</p>
           </div>
         </div>
         
         {/* Received Message */}
         <div className="flex justify-start">
           <div className="bg-gray-800 text-white p-3 rounded-lg max-w-xs md:max-w-md">
-            <p>I'm good, how about you?</p>
+            <p>US mai kaha kaha gaye ho ?</p>
           </div>
         </div>
         
@@ -22,14 +28,20 @@ const MessageContainer = () => {
         {/* Sent Message */}
         <div className="flex justify-end">
           <div className="bg-blue-700 text-white p-3 rounded-lg max-w-xs md:max-w-md">
-            <p>Any plans for the weekend?</p>
+            <p>Bohot jaga </p>
           </div>
         </div>
         
         {/* Received Message */}
         <div className="flex justify-start">
           <div className="bg-gray-800 text-white p-3 rounded-lg max-w-xs md:max-w-md">
-            <p>Thinking of going hiking!</p>
+            <p>naam bataya</p>
+          </div>
+          
+        </div>
+        <div className="flex justify-end">
+          <div className="bg-blue-700 text-white p-3 rounded-lg max-w-xs md:max-w-md">
+            <p> bhupendra jogi </p>
           </div>
         </div>
       </div>
