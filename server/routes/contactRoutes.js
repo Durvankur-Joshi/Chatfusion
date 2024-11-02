8b8c957b6cceb90339c4b7df11c3b704af30f937
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2024-11-02 12:37:35 +0530

    working on get contact

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyToken } from "../middleware/Authmiddleware.js";
-import { searchContact } from "../controllers/contactController.js";
+import { getContactForDMList, searchContact } from "../controllers/contactController.js";
 
 
 const contactRoutes = new Router();
 
 contactRoutes.post("/search" , verifyToken , searchContact);
+contactRoutes.get("/get-contacts-for-dm" , verifyToken , getContactForDMList);
 
 export default contactRoutes;
