906c5de1845a4c8c043d2a5c21c382eb10d2833b
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2024-11-02 12:36:53 +0530

    working on get contact

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -13,6 +13,7 @@ export const LOGOUT_ROUTES = `${AUTH_ROUTES}/logout`
 // contact constance 
 export const CONTACT_ROUTES = `/api/contacts`
 export const SEARCH_CONTACT = `${CONTACT_ROUTES}/search`
+export const GET_DM_CONTACT_ROUTES = "http://localhost:8080/api/contacts/get-contacts-for-dm"
 
 export const MESSAGES_ROUTES = "/api/messages";
 export const GET_ALL_MESSAGES_ROUTES = `${MESSAGES_ROUTES}/get-message`;
