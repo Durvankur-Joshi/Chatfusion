e2af66b6a7b7f851ad3f2c4b0a05e97142013c70
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2024-11-04 22:23:19 +0530

    server side done

--- server/routes/MessageRoutes.js
+++ server/routes/MessageRoutes.js
@@ -1,9 +1,26 @@
+// import { Router } from "express";
+// import { verifyToken } from "../middleware/Authmiddleware.js";
+// import { getMessage, uploadFile } from "../controllers/MessageController.js";
+// import multer from "multer";
+
+// const messageRoutes = Router();
+
+// const upload = multer({dest:"uploads/file"});
+// messageRoutes.post("/get-message" , verifyToken , getMessage);
+// messageRoutes.post("/upload-file" , verifyToken , upload.single("file" , uploadFile))
+
+// export default messageRoutes;
+
 import { Router } from "express";
+import multer from "multer";
 import { verifyToken } from "../middleware/Authmiddleware.js";
-import { getMessage } from "../controllers/MessageController.js";
+import { uploadFile, getMessage } from "../controllers/MessageController.js";
 
 const messageRoutes = Router();
 
-messageRoutes.post("/get-message" , verifyToken , getMessage);
+const upload = multer({ dest: "uploads/files" });
+
+messageRoutes.post("/get-message", verifyToken, getMessage);
+messageRoutes.post("/upload-file", verifyToken, upload.single("file"), uploadFile);
 
 export default messageRoutes;
