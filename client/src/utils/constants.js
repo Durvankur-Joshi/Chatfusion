67db11ad26ea492b2217fda1944117a13abbfc06
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2024-11-06 18:19:27 +0530

    update the component

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -14,6 +14,7 @@ export const LOGOUT_ROUTES = `${AUTH_ROUTES}/logout`
 export const CONTACT_ROUTES = `/api/contacts`
 export const SEARCH_CONTACT = `${CONTACT_ROUTES}/search`
 export const GET_DM_CONTACT_ROUTES = "http://localhost:8080/api/contacts/get-contacts-for-dm"
+export const GET_ALL_CONTACT_ROUTES = `${CONTACT_ROUTES}/get-all-contacts`
 
 export const MESSAGES_ROUTES = "/api/messages";
 export const GET_ALL_MESSAGES_ROUTES = `${MESSAGES_ROUTES}/get-message`;
