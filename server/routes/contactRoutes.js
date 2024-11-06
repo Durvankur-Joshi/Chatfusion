71c57412e8b384cd5dc499409283d98c89ecaeff
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2024-11-06 18:20:14 +0530

    update the routes

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyToken } from "../middleware/Authmiddleware.js";
-import { getContactForDMList, searchContact } from "../controllers/contactController.js";
+import { getAllContacts, getContactForDMList, searchContact } from "../controllers/contactController.js";
 
 
 const contactRoutes = new Router();
 
 contactRoutes.post("/search" , verifyToken , searchContact);
 contactRoutes.get("/get-contacts-for-dm" , verifyToken , getContactForDMList);
+contactRoutes.get("/get-all-contacts" , verifyToken , getAllContacts);
 
 export default contactRoutes;
