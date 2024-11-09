a0f1fadde96c3557db5173332e22298d458659da
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2024-11-09 20:40:53 +0530

    Update ChannelRoutes

--- server/routes/ChannelRoutes.js
+++ server/routes/ChannelRoutes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { verifyToken } from "../middleware/Authmiddleware.js";
-import { createChannel } from "../controllers/ChannelController.js";
+import { createChannel, getUserChannels , getChannelMessages } from "../controllers/ChannelController.js";
 
 const channelRoutes = Router();
 
 channelRoutes.post("/create-channel", verifyToken, createChannel);
+channelRoutes.get("/get-user-channel", verifyToken, getUserChannels);
+channelRoutes.get(
+    "/get-channel-messages/:channelId",
+    verifyToken,
+    getChannelMessages
+  );
 
 export default channelRoutes;
