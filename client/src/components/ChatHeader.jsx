b9b92b38925d7275c133e0edb3aa6e1665847627
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2024-11-09 20:40:43 +0530

    Update package-lock.json

--- client/src/components/ChatHeader.jsx
+++ client/src/components/ChatHeader.jsx
@@ -7,9 +7,9 @@ const ChatHeader = () => {
   const { closeChat, selectedChatData } = useAppStore();
 
   return (
-    <div className="bg-gray-900 p-4 flex items-center justify-between text-white  shadow-lg">
+    <div className="bg-gray-900 h-14 p-4  flex items-center justify-between text-white  shadow-lg">
       {/* Profile Section */}
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center space-x-4 mt-5">
         <div className="relative h-12 w-12 sm:h-16 sm:w-16 rounded-full overflow-hidden border-2 border-gray-700">
           {selectedChatData && selectedChatData.image ? (
             <img
@@ -18,19 +18,29 @@ const ChatHeader = () => {
               className="w-full h-full object-cover"
             />
           ) : (
-            <span className="flex items-center justify-center w-full h-full text-gray-500 text-sm bg-gray-700">No Image</span>
+            <span className="flex items-center justify-center w-full h-full text-2xl text-gray-950  bg-gray-700">{selectedChatData.name ? selectedChatData.name.charAt(0).toUpperCase() : "?"}</span>
           )}
+          {/* {contact.image ? (
+        <img
+            src={`${HOST}/${contact.image}`}
+            className="w-full h-full object-cover"
+            onError={(e) => { e.target.src = ""; }} // fallback image
+        />
+    ) : (
+        <span className="text-gray-950 font-bold text-sm sm:text-lg md:text-base">
+            {contact.name ? contact.name.charAt(0).toUpperCase() : "?"}
+        </span>
+    )} */}
         </div>
         {/* Contact Information */}
         <div className="text-left">
-          <p className="text-lg sm:text-xl font-bold">{selectedChatData?.firstname || "Unknown"}</p>
-          <p className="text-sm sm:text-base text-gray-400">{selectedChatData?.email || "No email"}</p>
+          <p className="text-lg sm:text-xl font-bold">{selectedChatData?.firstname || selectedChatData.name || "Unknown"}</p>
         </div>
       </div>
       
       {/* Close Button */}
       <button
-        className="btn btn-sm btn-circle btn-ghost hover:bg-red-600 hover:text-white transition-all duration-200 ease-in-out absolute right-4 top-4"
+        className="btn btn-sm btn-circle btn-ghost hover:bg-red-600 hover:text-white transition-all duration-200 ease-in-out absolute right-4 top-5"
         onClick={closeChat}>
         ✕
       </button>
