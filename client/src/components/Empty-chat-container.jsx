1c1b1c4402808300025c951c9176a6cdd62bcf5c
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2024-11-10 19:27:02 +0530

    Update Empty-chat-container component

--- client/src/components/Empty-chat-container.jsx
+++ client/src/components/Empty-chat-container.jsx
@@ -5,19 +5,20 @@ import AnimatedChatIcon from './AnimatedChatIcon';
 const EmptyChatContainer = () => {
   return (
     <>
-       <div className="flex flex-col items-center justify-center h-full bg-gray-100 text-gray-500">
-      {/* Icon */}
-      <AnimatedChatIcon />
-      
-      {/* Message */}
-      <h2 className="text-4xl ">
-        Hi! <span className=''>Welcome to </span> <span className='text-purple-800 font-bold'>ChatFusion</span>
-      </h2>
-      
-      <p className="mt-2 text-sm text-gray-400">
-        Your conversations will appear here once you choose a contact.
-      </p>
-    </div>
+      <div className="flex flex-col items-center justify-center h-full w-[80vw] bg-gray-900 text-gray-500">
+  {/* Icon */}
+  <AnimatedChatIcon />
+
+  {/* Message */}
+  <h2 className="text-4xl">
+    Hi! <span>Welcome to </span> <span className="text-purple-800 font-bold">ChatFusion</span>
+  </h2>
+
+  <p className="mt-2 text-sm text-gray-400">
+    Your conversations will appear here once you choose a contact.
+  </p>
+</div>
+
     </>
   )
 }
