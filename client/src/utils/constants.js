360a103ff27cda2e9ec6b1afeeb601503ae4fc98
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2024-11-10 19:27:07 +0530

    Update constants in utils

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -5,7 +5,7 @@ export const HOST = "http://localhost:8080";
 export const AUTH_ROUTES = `/api/auth`;
 export const SIGNUP_ROUTES = `${AUTH_ROUTES}/register`;
 export const LOGIN_ROUTES = `${AUTH_ROUTES}/login`;
-export const GET_USER_INFO = `${HOST}${AUTH_ROUTES}/user-info`;
+export const GET_USER_INFO = `http://localhost:8080/api/auth/user-info`;
 export const UPDATE_PROFILE_ROUTES = `${HOST}${AUTH_ROUTES}/update-profile`;
 export const ADD_PROFILE_IMAGE_ROUTES = `${HOST}${AUTH_ROUTES}/add-profile-image`;
 export const LOGOUT_ROUTES = `${AUTH_ROUTES}/logout`
@@ -13,7 +13,7 @@ export const LOGOUT_ROUTES = `${AUTH_ROUTES}/logout`
 // contact constance 
 export const CONTACT_ROUTES = `/api/contacts`
 export const SEARCH_CONTACT = `${CONTACT_ROUTES}/search`
-export const GET_DM_CONTACT_ROUTES = "http://localhost:8080/api/contacts/get-contacts-for-dm"
+export const GET_DM_CONTACT_ROUTES = `${CONTACT_ROUTES}/get-contacts-for-dm`
 export const GET_ALL_CONTACT_ROUTES = `${CONTACT_ROUTES}/get-all-contacts`
 
 export const MESSAGES_ROUTES = "/api/messages";
