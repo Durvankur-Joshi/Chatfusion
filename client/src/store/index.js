2b8a20d351d50270748b0f3cf0aebb2d55117da4
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2025-01-16 21:05:17 +0530

    Update store index.js

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,31 +1,4 @@
 
-
-
-
-
-// import {create} from 'zustand';
-
-// export const useAppStore = create((set) => ({
-//   userInfo: {
-//     firstName: '',
-//     lastName: '',
-//     profileColor: '',
-//     image: '',
-//     profileSetup: false,
-//   },
-//   setUserInfo: (userInfo) => set({ userInfo }),
-// }));
-// import {create} from "zustand";
-// import {createAuthSlices} from "./slices/auth-slice"
-// import { createChatSlice } from "./slices/chat-slice";
-
-
-// export const useAppStore = create()((...a)=>({
-//   ...createAuthSlices(...a),
-//   ...createChatSlice(...a),
-// }))
-
-
 // store.js or equivalent file
 import { create } from 'zustand';
 import { createAuthSlices } from './slices/auth-slice';
